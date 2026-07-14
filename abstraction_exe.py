66fad3c82af99be2bff7d58062606b5da2dc0036
chaim <chchdeu@gmail.com>
2026-07-14 16:44:17 +0300

    mission_10

--- abstraction_exe.py
+++ abstraction_exe.py
@@ -182,5 +182,86 @@ for r in resturants:
     print(f"{r.get_menu()}")
     r.prepare_order("burger")
 
-    
+#mission 10
+class Deliverymethod(ABC):
+    @abstractmethod
+    def deliver(self,order_id):
+        pass 
+    def get_eta(self):
+        pass
+    def get_cost(self,distance_km):
+        pass
+class Bikedelivery(Deliverymethod):
+    def deliver(self, order_id):
+        return f"order {order_id} dliverd by bike"
+    def get_eta(self):
+        return 30
+    def get_cost(self, distance_km):
+        return 3*distance_km
+class dronedelivery(Deliverymethod):
+    def deliver(self, order_id):
+        return f"order {order_id} dliverd by drone"
+    def get_eta(self):
+        return 10
+    def get_cost(self, distance_km):
+        return 12*distance_km
+class cardelivery(Deliverymethod):    
+    def deliver(self, order_id):
+        return f"order {order_id} dliverd by car"
+    def get_eta(self):
+        return 20
+    def get_cost(self, distance_km):
+        return 10*distance_km
+class walkingdelivery(Deliverymethod):
+    def deliver(self, order_id):
+        return f"order {order_id} dliverd by walk"
+    def get_eta(self):
+        return 45
+    def get_cost(self, distance_km):
+        return 2*distance_km
+# class platform:
+#     def __init__(self):
+#         self.optional_del=[Bikedelivery(),dronedelivery(),cardelivery(),walkingdelivery()]
+#     def cheapest_option(self,distance_km):
+#         lowes_cost=self.optional_del[0].get_cost(distance_km)
+#         for o in self.optional_del:
+#             if o.get_cost(distance_km)<lowes_cost:
+#                 lowes_cost=o.get_cost
+#         return lowes_cost
+#     def fastest_option(self):
+#         lowest_time=self.optional_del[0].get_eta()
+#         for l in self.optional_del:
+#             if l.get_eta() <lowest_time:
+#                 lowest_time=l.get_eta
+#         return lowest_time
 
+
+
+
+# a1=platform()
+# print(a1.cheapest_option(5))
+# print(a1.fastest_option())
+
+
+class Platform:
+    def __init__(self):
+        self.optional_del=[Bikedelivery(),dronedelivery(),cardelivery(),walkingdelivery()]
+    def cheapest_option(self, distance_km):
+        cheapest = self.optional_del[0]
+        lowest_cost = cheapest.get_cost(distance_km)
+        for d in self.optional_del:
+            if d.get_cost(distance_km) < lowest_cost:
+                cheapest = d
+                lowest_cost = d.get_cost(distance_km)
+        return cheapest
+    def fastest_option(self):
+        fastest = self.optional_del[0]
+        lowest_time = fastest.get_eta()
+        for d in self.optional_del:
+            if d.get_eta() < lowest_time:
+                fastest = d
+                lowest_time = d.get_eta()
+        return fastest
+platform = Platform()
+print(f"cheapast: {platform.cheapest_option(5).__class__.__name__}")
+print(f"fastest: {platform.fastest_option().__class__.__name__}")
