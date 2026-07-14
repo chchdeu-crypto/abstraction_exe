39155ffeeb374a753d19e943b9702ad606d495b9
chaim <chchdeu@gmail.com>
2026-07-14 11:39:46 +0300

    mission_2

--- abstraction_exe.py
+++ abstraction_exe.py
@@ -12,3 +12,23 @@ class Bikedelivery(DeliveryMethod):
         print(f"order {order_id} deliverd by bike")
 bike=Bikedelivery()
 bike.deliver(101)
+
+#mission 2
+class DeliveryMethod(ABC):
+    @abstractmethod
+    def delivery(self,order_id):
+        pass
+class Dronedelivery(DeliveryMethod):
+    def __init__(self):
+        super().__init__()
+    def delivery(self,order_id):
+        print(f"order {order_id} brought to your building by drone")
+class Cardelivery(DeliveryMethod):
+    def __init__(self):
+        super().__init__()
+    def delivery(self,order_id):
+        print(f"order {order_id} brought to your building by car")
+drone=Dronedelivery()
+drone.delivery(202)
+car=Cardelivery()
+car.delivery(202)
